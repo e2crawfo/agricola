74cbbd8478d3fb3709737c42124bda1d162b23db
ODA <twbtarai.blackpepper@gmail.com>
2019-07-06 16:08:46 +0900

    delete unused choice

--- agricola/choice.py
+++ agricola/choice.py
@@ -20,11 +20,13 @@ class Choice(object):
     def next_choices(self):
         return []
 
-# ActionChoise sample
-# {
-#   "action_id": "Fencing"
-# }
 class ActionChoice(Choice):
+    '''
+    ActionChoise sample
+    {
+        "action_id": "Fencing"
+    }
+    '''
     def __init__(self, game, player, choice_dict, desc=None, mx=None):
         super(ActionChoice, self).__init__(game, player, choice_dict)
         if "action_id" in choice_dict:
@@ -43,18 +45,6 @@ class OccupationChoice(Choice):
             self.choice_value = target_occupation[0]
         else:
             self.choice_value = None
-
-class AnimalMarketchoice(Choice):
-    def validate(self):
-        pass
-
-class Expansionchoice(Choice):
-    def validate(self):
-        pass
-
-class Stablechoice(Choice):
-    def validate(self):
-        pass
 
 class MinorImprovementChoice(Choice):
     def __init__(self, game, player, choice_dict, desc=None, mx=None):
@@ -83,30 +73,20 @@ class MajorImprovementChoice(Choice):
         else:
             self.choice_value = None
 
-class FarmlandChoice(Choice):
-    def validate(self):
-        pass
-
-class BakingChoice(Choice):
-    def validate(self):
-        pass
-
-class SowingChoice(Choice):
-    def validate(self):
-        pass
-
-# FencingChoice Sample
-# {
-#   "pastures": [
-#     [
-#       [4,1],
-#       [4,2],
-#       [3,1],
-#       [3,2]
-#     ]
-#   ]
-# }
 class FencingChoice(Choice):
+    '''
+     FencingChoice Sample
+    {
+        "pastures": [
+            [
+                [4,1],
+                [4,2],
+                [3,1],
+                [3,2]
+            ]
+        ]
+    }
+    '''
     def __init__(self, game, player, choice_dict, desc=None, mx=None):
         super(FencingChoice, self).__init__(game, player, choice_dict)
         if "pastures" in choice_dict:
@@ -114,52 +94,13 @@ class FencingChoice(Choice):
         else:
             self.choice_value = None
 
-# old choices
-
-class DictChoice(Choice):
-    def __init__(self, choice_dict, desc=None):
-        self.choice_dict = choice_dict
-        super(DictChoice, self).__init__(desc)
-
-class YesNoChoice(Choice):
-    def __init__(self, desc=None):
-        super(DiscreteChoice, self).__init__(desc)
-
-
-class DiscreteChoice(Choice):
-    def __init__(self, options, desc=None):
-        if not options:
-            raise ValueError(
-                "Cannot create a DiscreteChoice instance with an empty "
-                "options list. Choice description is:\n{0}".format(desc))
-
-        super(DiscreteChoice, self).__init__(desc)
-        self.options = list(options)
-
-
-class CountChoice(Choice):
-    def __init__(self, n=None, desc=None):
-        self.n = n
-        super(CountChoice, self).__init__(desc)
-
-
-class ListChoice(Choice):
-    def __init__(self, subchoices, desc=None):
-        super(ListChoice, self).__init__(desc)
-        self.subchoices = subchoices
-
-
-class VariableLengthListChoice(Choice):
-    def __init__(self, subchoice, desc=None, mx=None):
-        super(VariableLengthListChoice, self).__init__(desc)
-        self.subchoice = subchoice
-        self.mx = mx
-
-# SpaceChoice Sample
-# {
-#     "space": [4,0]
-# }
 class SpaceChoice(Choice):
+    '''
+    SpaceChoice input Sample
+    {
+        "space": [4,0]
+    }
+    '''
     def __init__(self, game, player, choice_dict, desc=None, mx=None):
         super(SpaceChoice, self).__init__(game, player, choice_dict)
         if "space" in choice_dict:
